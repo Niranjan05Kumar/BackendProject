15de72a9889e7db52e69ffdc56dfedc736b8ef22
Niranjan Kumar <niranjankumar112005@hotmail.com>
2025-07-23 18:20:57 +0530

    implement error handling in like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -9,141 +9,157 @@ import { Comment } from "../models/comment.model.js";
 import { Tweet } from "../models/tweet.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const { videoId } = req.params;
-    const userId = req.user._id;
-    if (!isValidObjectId(userId)) {
-        throw new ApiError(400, "Invalid user ID");
-    }
-    if (!isValidObjectId(videoId)) {
-        throw new ApiError(400, "Invalid video ID");
-    }
-    const like = await Like.findOne({
-        likedBy: userId,
-        video: videoId,
-    });
-    if (like) {
-        await Like.findByIdAndDelete(like._id);
+    try {
+        const { videoId } = req.params;
+        const userId = req.user._id;
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid user ID");
+        }
+        if (!isValidObjectId(videoId)) {
+            throw new ApiError(400, "Invalid video ID");
+        }
+        const like = await Like.findOne({
+            likedBy: userId,
+            video: videoId,
+        });
+        if (like) {
+            await Like.findByIdAndDelete(like._id);
+            return res
+                .status(200)
+                .json(new ApiResponce(200, null, "Like removed successfully"));
+        }
+
+        const newLike = await Like.create({
+            likedBy: userId,
+            video: videoId,
+        });
+        if (!newLike) {
+            throw new ApiError(500, "Failed to add like");
+        }
+
         return res
             .status(200)
-            .json(new ApiResponce(200, null, "Like removed successfully"));
+            .json(new ApiResponce(200, null, "Like added successfully"));
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, error.message));
     }
-
-    const newLike = await Like.create({
-        likedBy: userId,
-        video: videoId,
-    });
-    if (!newLike) {
-        throw new ApiError(500, "Failed to add like");
-    }
-
-    return res
-        .status(200)
-        .json(new ApiResponce(200, null, "Like added successfully"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const { commentId } = req.params;
-    if (!isValidObjectId(commentId)) {
-        throw new ApiError(400, "Invalid comment ID");
-    }
-    const userId = req.user._id;
-    const like = await Like.findOne({
-        likedBy: userId,
-        comment: commentId,
-    });
-    if (like) {
-        await Like.findByIdAndDelete(like._id);
+    try {
+        const { commentId } = req.params;
+        if (!isValidObjectId(commentId)) {
+            throw new ApiError(400, "Invalid comment ID");
+        }
+        const userId = req.user._id;
+        const like = await Like.findOne({
+            likedBy: userId,
+            comment: commentId,
+        });
+        if (like) {
+            await Like.findByIdAndDelete(like._id);
+            return res
+                .status(200)
+                .json(new ApiResponce(200, null, "Like removed successfully"));
+        }
+        const newLike = await Like.create({
+            likedBy: userId,
+            comment: commentId,
+        });
+        if (!newLike) {
+            throw new ApiError(500, "Failed to add like");
+        }
         return res
             .status(200)
-            .json(new ApiResponce(200, null, "Like removed successfully"));
+            .json(new ApiResponce(200, null, "Like added successfully"));
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, error.message));
     }
-    const newLike = await Like.create({
-        likedBy: userId,
-        comment: commentId,
-    });
-    if (!newLike) {
-        throw new ApiError(500, "Failed to add like");
-    }
-    return res
-        .status(200)
-        .json(new ApiResponce(200, null, "Like added successfully"));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const { tweetId } = req.params;
-    if (!isValidObjectId(tweetId)) {
-        throw new ApiError(400, "Invalid tweet ID");
-    }
-    const userId = req.user._id;
-    const like = await Like.findOne({
-        likedBy: userId,
-        tweet: tweetId,
-    });
-    if (like) {
-        await Like.findByIdAndDelete(like._id);
+    try {
+        const { tweetId } = req.params;
+        if (!isValidObjectId(tweetId)) {
+            throw new ApiError(400, "Invalid tweet ID");
+        }
+        const userId = req.user._id;
+        const like = await Like.findOne({
+            likedBy: userId,
+            tweet: tweetId,
+        });
+        if (like) {
+            await Like.findByIdAndDelete(like._id);
+            return res
+                .status(200)
+                .json(new ApiResponce(200, null, "Like removed successfully"));
+        }
+        const newLike = await Like.create({
+            likedBy: userId,
+            tweet: tweetId,
+        });
+        if (!newLike) {
+            throw new ApiError(500, "Failed to add like");
+        }
         return res
             .status(200)
-            .json(new ApiResponce(200, null, "Like removed successfully"));
+            .json(new ApiResponce(200, null, "Like added successfully"));
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, error.message));
     }
-    const newLike = await Like.create({
-        likedBy: userId,
-        tweet: tweetId,
-    });
-    if (!newLike) {
-        throw new ApiError(500, "Failed to add like");
-    }
-    return res
-        .status(200)
-        .json(new ApiResponce(200, null, "Like added successfully"));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    const userId = req.user._id;
-    if (!isValidObjectId(userId)) {
-        throw new ApiError(400, "Invalid user ID");
-    }
-    const likedVideos = await User.aggregate([
-        {
-            $match: {
-                _id: new mongoose.Types.ObjectId(userId),
+    try {
+        const userId = req.user._id;
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid user ID");
+        }
+        const likedVideos = await User.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(userId),
+                },
             },
-        },
-        {
-            $lookup: {
-                from: "likes",
-                localField: "_id",
-                foreignField: "likedBy",
-                as: "likes",
+            {
+                $lookup: {
+                    from: "likes",
+                    localField: "_id",
+                    foreignField: "likedBy",
+                    as: "likes",
+                },
             },
-        },
-        { $unwind: "$likes" },
-        {
-            $lookup: {
-                from: "videos",
-                localField: "likes.video",
-                foreignField: "_id",
-                as: "videoDetails",
+            { $unwind: "$likes" },
+            {
+                $lookup: {
+                    from: "videos",
+                    localField: "likes.video",
+                    foreignField: "_id",
+                    as: "videoDetails",
+                },
             },
-        },
-        { $unwind: "$videoDetails" },
-        {
-            $project: {
-                _id: 0,
-                video: "$videoDetails",
+            { $unwind: "$videoDetails" },
+            {
+                $project: {
+                    _id: 0,
+                    video: "$videoDetails",
+                },
             },
-        },
-        { $sort: { "videoDetails.createdAt": -1 } },
-    ]);
+            { $sort: { "videoDetails.createdAt": -1 } },
+        ]);
 
-    return res
-        .status(200)
-        .json(
-            new ApiResponce(
-                200,
-                likedVideos,
-                "Liked videos fetched successfully"
-            )
-        );
+        return res
+            .status(200)
+            .json(
+                new ApiResponce(
+                    200,
+                    likedVideos,
+                    "Liked videos fetched successfully"
+                )
+            );
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, error.message));
+    }
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
