3ec46a24c8e878300988b750d4a20219f4947981
Niranjan Kumar <niranjankumar112005@hotmail.com>
2025-07-24 11:39:01 +0530

    Refactor subscription controller to improve error handling and response messages

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,76 +6,90 @@ import { ApiResponce } from "../utils/apiResponce.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const { channelId } = req.params;
-    if (!isValidObjectId(channelId)) {
-        throw new ApiError(400, "Invalid channel ID.");
-    }
+    try {
+        const { channelId } = req.params;
+        if (!isValidObjectId(channelId)) {
+            throw new ApiError(400, "Invalid channel ID.");
+        }
 
-    const user = await User.findById(req.user._id);
-    if (!user) {
-        throw new ApiError(404, "User not found.");
-    }
+        const user = await User.findById(req.user._id);
+        if (!user) {
+            throw new ApiError(404, "User not found.");
+        }
 
-    const subscription = await Subscription.findOne({
-        subscriber: user._id,
-        channel: channelId,
-    });
+        const subscription = await Subscription.findOne({
+            subscriber: user._id,
+            channel: channelId,
+        });
 
-    if (subscription) {
-        await Subscription.deleteOne({ _id: subscription._id });
+        if (subscription) {
+            await Subscription.deleteOne({ _id: subscription._id });
+            return res
+                .status(200)
+                .json(new ApiResponce(200, null, "Unsubscribed successfully"));
+        }
+
+        const newSubscription = await Subscription.create({
+            subscriber: user._id,
+            channel: channelId,
+        });
         return res
-            .status(200)
-            .json(new ApiResponce(200, null, "Unsubscribed successfully"));
+            .status(201)
+            .json(
+                new ApiResponce(201, newSubscription, "Subscribed successfully")
+            );
+    } catch (error) {
+        throw new ApiError(500, "Internal server error.");
     }
-
-    const newSubscription = await Subscription.create({
-        subscriber: user._id,
-        channel: channelId,
-    });
-    return res
-        .status(201)
-        .json(new ApiResponce(201, newSubscription, "Subscribed successfully"));
 });
 
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const { channelId } = req.params;
-    if (!isValidObjectId(channelId)) {
-        throw new ApiError(400, "Invalid channel ID.");
+    try {
+        const { channelId } = req.params;
+        if (!isValidObjectId(channelId)) {
+            throw new ApiError(400, "Invalid channel ID.");
+        }
+        const subscribers = await Subscription.find({ channel: channelId });
+        if (!subscribers) {
+            throw new ApiError(404, "No subscribers found for this channel.");
+        }
+        return res
+            .status(200)
+            .json(
+                new ApiResponce(
+                    200,
+                    subscribers,
+                    "Subscribers fetched successfully"
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, "Internal server error.");
     }
-    const subscribers = await Subscription.find({ channel: channelId });
-    if (!subscribers) {
-        throw new ApiError(404, "No subscribers found for this channel.");
-    }
-    return res
-        .status(200)
-        .json(
-            new ApiResponce(
-                200,
-                subscribers,
-                "Subscribers fetched successfully"
-            )
-        );
 });
 
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const userId = req.user._id;
-    const subscriptions = await Subscription.find({ subscriber: userId });
+    try {
+        const userId = req.user._id;
+        const subscriptions = await Subscription.find({ subscriber: userId });
 
-    if (!subscriptions || subscriptions.length === 0) {
+        if (!subscriptions || subscriptions.length === 0) {
+            return res
+                .status(404)
+                .json(new ApiError(404, "No subscribed channels found."));
+        }
+
         return res
-            .status(404)
-            .json(new ApiError(404, "No subscribed channels found."));
+            .status(200)
+            .json(
+                new ApiResponce(
+                    200,
+                    subscriptions,
+                    "Subscribed channels fetched successfully"
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, "Internal server error.");
     }
-
-    return res
-        .status(200)
-        .json(
-            new ApiResponce(
-                200,
-                subscriptions,
-                "Subscribed channels fetched successfully"
-            )
-        );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
